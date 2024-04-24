38bcc8d4f27d7976f73b1df3a87a913df9c18822
TelRich <okorogoodrich@gmail.com>
2024-04-24 13:01:17 +0100

    renamed and adjusted context

--- Novotern.py
+++ Novotern.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align:center;'>Interest Rate Spread</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align:center;'>NOVOTERN EDTECH PORTFOLIO</h1>", unsafe_allow_html=True)
 
 st.markdown("""<center>
 Welcome to Novotern Portfolio          
