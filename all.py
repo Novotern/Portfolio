5f7ab773b17e224e45af2f5afeccee99c1fb0237
TelRich <okorogoodrich@gmail.com>
2024-04-24 12:42:01 +0100

    renamed file

---
+++ all.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align:center;'>Interest Rate Spread</h1>", unsafe_allow_html=True)
+
+st.markdown("""<center>
+Welcome to Novotern Portfolio          
+</center>""", unsafe_allow_html=True)
---
+++ pages/Loan_Metrics.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align:center;'>Nigeria Farmland Analysis</h1>", unsafe_allow_html=True)
+
+st.markdown("""<center>
+
+# Interest Rate Spread
+
+Interest Rate Spread is a fundamental measure used in the finance and banking industry to evaluate the profitability of lending activities. It represents the difference between the interest rate earned on loans extended by a financial institution and the interest rate paid on the deposits it holds or other funding sources.
+
+Interest Rate Spread = Loan Interest Rate - Deposit Interest Rate
+
+The interest rate spread is a crucial metric for assessing the profitability and efficiency of lending operations. A positive spread indicates that the institution is earning more from its loans than it is paying on its funding sources, resulting in net interest income. Conversely, a negative spread suggests that the institution may be paying out more in interest than it is earning from its loans, potentially leading to reduced profitability or losses.
+
+Financial institutions carefully manage their interest rate spread to ensure that it is sufficient to cover operating expenses, credit risk, loan loss provisions, and provide an adequate return to shareholders. Changes in market interest rates, economic conditions, and regulatory policies can impact the interest rate spread, making it essential for institutions to monitor and adjust their lending practices accordingly.
+
+In summary, the interest rate spread is a key metric used by financial institutions to assess the profitability and risk of their lending activities, providing insights into their overall financial health and performance.          
+</center>""", unsafe_allow_html=True)
