126a8827547ea42432a9feb7de63df6f8be8a30c
Goodrich <novoternhq@gmail.com>
2024-05-07 16:30:25 +0100

    renamed section

--- pages/panel_analysis.py
+++ pages/panel_analysis.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
+st.set_page_config(layout="wide", page_title='Finance KPIs', page_icon='logo.png' ) 
 
-with st.expander(':red[Health Industry]', expanded=True):
+
+with st.expander(':red[Finance KPIs]', expanded=True):
   power = """<iframe title="Report Section" width="1200" height="1000" 
               src="https://app.powerbi.com/view?r=eyJrIjoiMGVlOGFiYzYtZjM0MC00ZmNmLWJlYjAtNGVmOTNhY2E0ZGNjIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
               frameborder="0" allowFullScreen="true"></iframe>"""
